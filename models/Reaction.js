8150cdfea087caf0564edafde6d9c29e673fba47
Jiang Jili <jiangjili@gmail.com>
2022-11-06 20:53:20 -0800

    create thought

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+const { Schema, Types } = require('mongoose');
+
+// Reaction schema
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now, // need getter to format date
+    },
+  }
+);
+
+module.exports = reactionSchema;
